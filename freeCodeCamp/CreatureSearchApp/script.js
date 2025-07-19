d3e2d21fd9bcc2c1033a23d1bfe490532f2e78f9
Anjanee Wijewardana <anjaneew@gmail.com>
2025-07-19 12:18:32 +0300

    freeCodeCamp RPG Creature Search App

--- freeCodeCamp/CreatureSearchApp/script.js
+++ freeCodeCamp/CreatureSearchApp/script.js
@@ -4,6 +4,8 @@
 const searchInput  = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-button");
 const creatureNameText = document.getElementById("creature-name");
+const specialNameText = document.getElementById("special-name");
+const specialDescription = document.getElementById("special-description");
 const creatureIdText  = document.getElementById("creature-id");
 const weightText = document.getElementById("weight");
 const heightText = document.getElementById("height");
@@ -19,13 +21,52 @@ const fetchData = async () => {
     try {
        console.log("button clicked"); 
        const inputText = searchInput.value.toLowerCase().trim();
-       const res = await fetch(`https://rpg-creature-api.freecodecamp.rocks/api/creature/${inputText}`);
-       const data = await res.json();
-       console.log(data);
+       const response = await fetch(`https://rpg-creature-api.freecodecamp.rocks/api/creature/${inputText}`);
+       if(!response.ok){
+        alert("Creature not found");
+        return;
+       }
+       else{
+        const data = await response.json();
+        console.log(data);
+        updateDisplay(data);
+       }
 
     } catch (error) {
         console.error(error);
     }
+};
+
+const updateDisplay = (data) =>{
+    
+
+    const {id, name, weight, height, special, types, stats} = data;
+
+    //---------update display----------
+    creatureNameText.innerText = name;
+    specialNameText.innerText = special.name;
+    specialDescription.innerText = special.description;
+    creatureIdText.innerText = id;
+    weightText.innerText = weight;
+    heightText.innerText = height;
+    hpText.innerText = stats[0].base_stat;
+    attackText.innerText = stats[1].base_stat;
+    defenseText.innerText = stats[2].base_stat;
+    specialAttackText.innerText = stats[3].base_stat;
+    specialDefenseText.innerText = stats[4].base_stat;
+    speedText.innerText =  stats[5].base_stat;
+
+    typesText.innerHTML = "";
+
+    // typesText.innerText = types[0].name.toUpperCase(); 
+    types.forEach(t => {
+        const span = document.createElement("span");
+        span.innerText = t.name.toUpperCase();
+        typesText.appendChild(span);
+    });
+
+
+
 };
 
 searchBtn.addEventListener("click", fetchData);
